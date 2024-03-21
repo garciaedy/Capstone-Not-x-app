b8f5db1292f68c282ee4e88c0bc9517ad41c1e68
garciaedy <edygarcia97@gmail.com>
2024-03-21 13:42:42 -0400

    Initial commit

---
+++ src/test/java/com/capstone/notxapp/NotXAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.notxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotXAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
